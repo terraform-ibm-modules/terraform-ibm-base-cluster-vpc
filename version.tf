60867f07c0b62eec579af9899af93e0889d61b33
Aditya-Ranjan3 <aditya.ranjan3@ibm.com>
2026-07-27 12:36:25 +0530

    fix

--- version.tf
+++ version.tf
@@ -4,7 +4,7 @@ terraform {
     # Use "greater than or equal to" range in modules
     ibm = {
       source  = "ibm-cloud/ibm"
-      version = ">= 1.86.0, < 3.0.0"
+      version = ">= 2.1.0, < 3.0.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
